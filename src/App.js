e98f0f40efb200d1c7bf2400c98f9a78364caf04
Enzosanchezz <enzosanchez771@gmail.com>
2022-12-16 15:03:27 -0300

    agregue estilos y un header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import firebase from './config/firebase';
 import "firebase/auth"
 import "./app.css";
-import Form from "./components/Form/Form";
 import Home from './components/Home/Home';
 import SearchBar from './components/SearchBar/SearchBar';
 import Header from './components/Header/Header';
+
 
 
 function App() {
 
   return (
     <div className='App' >
-      <Header />
+      <Header title="Registro de empresas" subtitle="" />
       <SearchBar />
       <Home />
     </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import style from "./header.module.css";
+import logo from "../../Assets/logoTransparente.png"
 
-export default function Header() {
+export default function Header(props) {
   return (
-    <div>
-        
+    <div className={style.div} >
+      <div className={style.header}>
+        <img className={style.logo} src={logo} alt="Logo" />
+        <h1>{props.title}</h1>
+      </div>
     </div>
-  )
+  );
 }
