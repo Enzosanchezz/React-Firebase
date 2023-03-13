59f1f13a833ece45c333860ab446f8f85ee134cc
Enzosanchezz <enzosanchez771@gmail.com>
2023-03-13 20:34:20 -0300

    delete app.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import firebase from './config/firebase';
 import "firebase/auth"
-import "./app.css";
 import Home from './components/Home/Home';
 import SearchBar from './components/SearchBar/SearchBar';
 import Header from './components/Header/Header';
